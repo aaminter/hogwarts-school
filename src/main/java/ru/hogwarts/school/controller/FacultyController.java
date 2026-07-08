b696216067229e6d27cca6158c7d044db0e251c2
aaminter <aam.interpolyaris@gmail.com>
2026-07-08 20:02:31 +0300

    Add REST controllers

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
+    @PostMapping
+    public Faculty createFaculty(@RequestBody Faculty faculty) {
+        return facultyService.createFaculty(faculty);
+    }
+
+    @GetMapping("{id}")
+    public Faculty getFaculty(@PathVariable Long id) {
+        return facultyService.getFaculty(id);
+    }
+
+    @PutMapping
+    public Faculty editFaculty(@RequestBody Faculty faculty) {
+        return facultyService.editFaculty(faculty);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteFaculty(@PathVariable Long id) {
+        facultyService.deleteFaculty(id);
+    }
+
+    @GetMapping
+    public List<Faculty> getAllFaculties() {
+        return facultyService.getAllFaculties();
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,41 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.repository.FacultyRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FacultyService {
+
+    private final FacultyRepository facultyRepository;
+
+    @Autowired
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty createFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty getFaculty(Long id) {
+        Optional<Faculty> faculty = facultyRepository.findById(id);
+        return faculty.orElse(null);
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(Long id) {
+        facultyRepository.deleteById(id);
+    }
+
+    public List<Faculty> getAllFaculties() {
+        return facultyRepository.findAll();
+    }
+}
